0c8777b1f6d672b1e5bbb636e5737c58c3924dc4
Leonides <xxluckyleoxx1@gmail.com>
2021-05-17 19:24:40 -0400

    commit tree 786603d4b9a137ef83b56a6d1697d54e577e494d

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+    my_array = []
+    my_array
 end
 
 def create_an_array
-  
+    my_array = ["Leo1", "Leo2", "Leo3", "Leo4"]
+    my_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array[-1]
 end
